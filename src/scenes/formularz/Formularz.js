0aca9a27503686d734d3cb844d90f65f4122b789
Karol Zięba <kz516749@gmail.com>
2022-01-02 03:07:41 +0100

    Poprawienie formularza, tworzenie plikow .json

--- src/scenes/formularz/Formularz.js
+++ src/scenes/formularz/Formularz.js
@@ -1,69 +1,140 @@
 import React, { useState } from 'react'
+import * as FileSystem from 'expo-file-system';
 import PropTypes from 'prop-types'
 import {
   StyleSheet, Text, View, StatusBar,
 } from 'react-native'
-// import Button from 'components/Button'
-import { Button, Input as TextInput } from 'native-base'
 import { colors } from 'theme'
-// import { TextInput } from 'react-native-gesture-handler'
 import { SafeAreaView } from 'react-native-safe-area-context'
-
+import { Button, Input as TextInput, Stack } from 'native-base'
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     backgroundColor: colors.lightGrayPurple,
   },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
+  text1: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginTop: 50,
   },
+  text2: {
+    fontSize: 15,
+    marginTop: 50,
+  },
+  input: {
+    marginTop: 12.5,
+  }
 })
 
 const Formularz = ({ route, navigation }) => {
+  const [imie, setImie]= useState("");
+  const [nazwisko, setNazwisko] = useState("");
+  const [nrPolisy, setNrPolisy] = useState("");
+  const [dataDodania, setDataDodania] = useState("");
 
-  const [imie,setImie]= useState("");
-  const [nazwisko,setNazwisko] = useState("");
-  const [nrPolisy,setNrPolisy] = useState("");
   const from = route?.params?.from
   return (
-  <SafeAreaView>
-      <View style={styles.root}>
-      
-        <Text>podaj imie </Text>
-        <TextInput value = {imie} placeholder="Imie" onChangeText={(newText) => {
+  <SafeAreaView style = {styles.root}>
+    <TextInput
+      style = {styles.input}
+      size = "lg"
+      mx = "3"
+      value = {imie} 
+      placeholder = "Imię"
+      w = {{
+        base: "75%",
+        md: "25%",
+      }}
+      onChangeText={(newText) => {
           setImie(newText)
-        }} ></TextInput>
-    
-        <Text>podaj nazwisko </Text>
-        <TextInput value={nazwisko} placeholder="nazwisko" onChangeText={(newText) => {
+      }}
+    />
+
+    <TextInput
+      style = {styles.input}
+      size = "lg"
+      mx = "3"
+      value = {nazwisko} 
+      placeholder = "Nazwisko"
+      w = {{
+        base: "75%",
+        md: "25%",
+      }}
+      onChangeText={(newText) => {
           setNazwisko(newText)
-        }}></TextInput>
-        <Text>podaj numer polisy </Text>
-        <TextInput value={nrPolisy} placeholder="nr Polisy" keyboard="numeric" onChangeText= {(newText) => {
+      }}
+    />
+
+    <TextInput
+      style = {styles.input}
+      size = "lg"
+      mx = "3"
+      value = {nrPolisy} 
+      placeholder = "Numer polisy"
+      w = {{
+        base: "75%",
+        md: "25%",
+      }}
+      onChangeText={(newText) => {
           setNrPolisy(newText)
-        }}></TextInput>
+      }}
+    />
 
-    <Text> {imie}</Text>
-    <Text> {nazwisko}</Text>
-    <Text> {nrPolisy}</Text>
+    <Button 
+      style = {styles.input}
+      size = "md"
+      backgroundColor = {colors.darkPurple} 
+      onPress = {() => {
 
-  <Button marginBottom="2px">Dodaj</Button>
-  <Button marginBottom="2px" onPress = {() => {
-    setImie("");
-    setNazwisko("")
-    setNrPolisy("")
-  }}>clear</Button>
-        <Button 
-          onPress={navigation.goBack}
-        >
-          powrót do strony startowej
-        </Button>
-      </View>
+      }}>
+    ZRÓB ZDJĘCIE</Button>
+
+    <Button 
+      style = {styles.input}
+      size = "md"
+      backgroundColor = {colors.darkPurple} 
+      onPress = {() => {
+        if(imie != "" && nazwisko != "" && nrPolisy != "") {
+          setDataDodania(new Date(Date.now()));
+          var sciezka = FileSystem.documentDirectory + (imie + "_" + nazwisko + "_" + nrPolisy + "_" + dataDodania + ".json");
+
+          FileSystem.writeAsStringAsync(sciezka, "{\"imie\": \"" + imie + "\",\n\"nazwisko\": \"" + nazwisko + "\",\n\"nrPolisy\": \"" + nrPolisy + "\",\n\"dataDodania\": \"" + dataDodania + "\"}");
+
+          var dodanaPolisa = FileSystem.readAsStringAsync(sciezka);
+          dodanaPolisa.then((zawartosc) => {
+            var dane = JSON.parse(zawartosc);
+            alert('Dodano polisę o następujących danych:\n' + "Imię: " + dane.imie + "\nNazwisko: " + dane.nazwisko + "\nNumer polisy: " + dane.nrPolisy + "\nData dodania: " + dane.dataDodania);
+          });
+
+          setImie("");
+          setNazwisko("");
+          setNrPolisy("");
+        }
+      }}>
+    DODAJ POLISĘ</Button>
+
+    <Button 
+      style = {styles.input}
+      size = "md"
+      backgroundColor = {colors.darkPurple} 
+      onPress = {() => {
+        setImie("");
+        setNazwisko("");
+        setNrPolisy("");
+      }}>
+    WYCZYŚĆ FORMULARZ</Button>
+
+    <Button 
+      style = {styles.input}
+      size = "md"
+      backgroundColor = {colors.darkPurple} 
+      onPress = {navigation.goBack}>
+    POWRÓT DO STRONY GŁÓWNEJ</Button>
+    
   </SafeAreaView>
   )
 }
