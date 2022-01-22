796082656c36a05f25788c1b92c9da49ddcca0b2
Karol Zięba <kz516749@gmail.com>
2022-01-22 17:59:41 +0100

    Dodanie działającego aparatu w formularzu

--- src/scenes/camera/Camera.js
+++ src/scenes/camera/Camera.js
@@ -1,29 +1,102 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types'
-import {
-  StyleSheet, Text, View, StatusBar,
-} from 'react-native'
-import Button from 'components/Button'
-import { colors } from 'theme'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { StyleSheet, Text, View, TouchableOpacity, Button, Image } from 'react-native';
+import { Camera as Aparat } from 'expo-camera';
 
 const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: colors.lightGrayPurple,
-  },
-  
-})
+    container: {
+      flex: 1,
+    },
+    camera: {
+      flex: 1,
+    },
+    buttonContainer: {
+      flex: 1,
+      backgroundColor: 'transparent',
+      flexDirection: 'row',
+      margin: 20,
+    },
+    button: {
+      flex: 0.2,
+      alignSelf: 'flex-end',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 20,
+      color: 'white',
+    },
+    stretch: {
+      width: 400,
+      height: 400,
+      resizeMode: 'stretch',
+    },
+  });
 
 const Camera = ({ route, navigation }) => {
-    return (
-    <SafeAreaView style = {styles.root}>
-      
-    </SafeAreaView>
-    )
+  const [pokazAparat, setPokazAparat] = useState(true)
+  const [dostep, setDostep] = useState(null);
+  const [typ, setTyp] = useState(Aparat.Constants.Type.back);
+  const [zdjecie, setZdjecie] = useState("");
+  const aparatRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Aparat.requestCameraPermissionsAsync();
+      setDostep(status === 'granted');
+    })();
+  }, []);
+
+  if (dostep === null) {
+    return <View />;
+  }
+  if (dostep === false) {
+    return <Text>Brak dostępu do kamery!</Text>;
+  }
+
+  const zrobZdjecie = async () => {
+    if(aparatRef) {
+      try {
+        let zdj = await aparatRef.current.takePictureAsync({
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+
+        return zdj;
+      }
+      catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {pokazAparat ?
+      (<Aparat style={styles.camera} type={typ} ref={aparatRef}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={async () => {
+              const zdj = await zrobZdjecie();
+              setZdjecie(zdj.uri);
+              
+              setPokazAparat(false);
+            }
+            }>
+            <Text style={styles.text}>Zrób zdjęcie</Text>
+          </TouchableOpacity>
+        </View>
+      </Aparat>)
+
+      :
+
+      (<View>
+          <Image style={styles.stretch} source={{uri: zdjecie}}/>
+      </View>)
+      }
+    </View>
+  );
   }
 
 Camera.propTypes = {
